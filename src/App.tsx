ec268964f1ee2c4c85a644d81ded2dafc695adac
PrKen <duflaut@et.esiea.fr>
2025-02-10 00:04:15 +0100

    app error handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Politique from "./pages/Politique";
@@ -16,16 +16,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <Router basename="/">
         <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/contact-form" element={<ContactForm />} />
           <Route path="/politique" element={<Politique />} />
-          {/* CATCH-ALL ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          {/* Redirection automatique pour les erreurs 404 */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </TooltipProvider>
   </QueryClientProvider>
 );
